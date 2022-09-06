788c912436e4b9fe4cc5143b49140a028248452e
Tim Kim <timk@adobe.com>
2022-09-06 13:41:08 -0700

    Update path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@
  */
 
 module.exports = {
-  pathPrefix: process.env.PATH_PREFIX || '/dev-site-documentation-template/',
+  pathPrefix: process.env.PATH_PREFIX || '/indesign/uxp/scripting/',
   siteMetadata: {
     versions: [
       {
